5052a08374ffae827028181810ed1c1f6ce81024
Jeremy Poulter <jeremy@global-dlna.org>
2016-08-09 21:02:02 +0100

    Initial version, basically the example with some mDNS thrown in

---
+++ WiFiTelnetToSerial.ino
@@ -0,0 +1,109 @@
+/* 
+  WiFiTelnetToSerial - Example Transparent UART to Telnet Server for esp8266
+
+  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
+  This file is part of the ESP8266WiFi library for Arduino environment.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+
+//how many clients should be able to telnet to this ESP8266
+#define MAX_SRV_CLIENTS 5
+
+#define HOSTNAME "espserial"
+
+#define DEBUG Serial1
+
+const char* ssid = "wibble";
+const char* password = "TheB1gJungle2";
+
+WiFiServer server(23);
+WiFiClient serverClients[MAX_SRV_CLIENTS];
+
+void setup() {
+  DEBUG.begin(115200);
+  WiFi.begin(ssid, password);
+  DEBUG.print("\nConnecting to "); DEBUG.println(ssid);
+  uint8_t i = 0;
+  while (WiFi.status() != WL_CONNECTED && i++ < 20) delay(500);
+  if(i == 21){
+    DEBUG.print("Could not connect to"); DEBUG.println(ssid);
+    while(1) delay(500);
+  }
+  //start UART and the server
+  Serial.begin(115200);
+  server.begin();
+  server.setNoDelay(true);
+  
+  DEBUG.print("Ready! Use 'telnet ");
+  DEBUG.print(WiFi.localIP());
+  DEBUG.println(" 23' to connect");
+
+  
+  // Set up mDNS responder:
+  // - first argument is the domain name, in this example
+  //   the fully-qualified domain name is "esp8266.local"
+  // - second argument is the IP address to advertise
+  //   we send our IP address on the WiFi network
+  if (MDNS.begin(HOSTNAME)) {
+    DEBUG.println("mDNS responder started");
+    // Add service to MDNS-SD
+    MDNS.addService("telnet", "tcp", 23);
+  }
+}
+
+void loop() {
+  uint8_t i;
+  //check if there are any new clients
+  if (server.hasClient()){
+    for(i = 0; i < MAX_SRV_CLIENTS; i++){
+      //find free/disconnected spot
+      if (!serverClients[i] || !serverClients[i].connected()){
+        if(serverClients[i]) serverClients[i].stop();
+        serverClients[i] = server.available();
+        serverClients[i].write("Connected!\r\n");
+        DEBUG.print("New client: "); DEBUG.print(i);
+        continue;
+      }
+    }
+    //no free/disconnected spot so reject
+    WiFiClient serverClient = server.available();
+    serverClient.stop();
+  }
+  //check clients for data
+  for(i = 0; i < MAX_SRV_CLIENTS; i++){
+    if (serverClients[i] && serverClients[i].connected()){
+      if(serverClients[i].available()){
+        //get data from the telnet client and push it to the UART
+        while(serverClients[i].available()) Serial.write(serverClients[i].read());
+      }
+    }
+  }
+  //check UART for data
+  if(Serial.available()){
+    size_t len = Serial.available();
+    uint8_t sbuf[len];
+    Serial.readBytes(sbuf, len);
+    //push UART data to all connected telnet clients
+    for(i = 0; i < MAX_SRV_CLIENTS; i++){
+      if (serverClients[i] && serverClients[i].connected()){
+        serverClients[i].write(sbuf, len);
+        delay(1);
+      }
+    }
+  }
+}
